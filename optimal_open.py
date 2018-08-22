de71b12602a204b2784cca35f25b0bb0b59bb634
David Lu <davidboxboro@gmail.com>
2018-08-22 10:55:13 -0400

    Update optimal_open.py

--- optimal_open.py
+++ optimal_open.py
@@ -1,7 +1,3 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
 # Optimal attacker TPR, FPR, precision, F1 score -- open-world
 
 import math
